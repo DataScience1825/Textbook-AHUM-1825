b353f8037be55b9d5dd19c75afaa774177459d7d
SchneiderCode <schneidm@goldmail.etsu.edu>
2022-08-13 21:21:56 -0400

    fix file path

--- inst/tutorials/SharedResources/utils.R
+++ inst/tutorials/SharedResources/utils.R
@@ -7,7 +7,7 @@ shared_resources_dir <- "~/AHUM-1825-Textbook/SharedResources" # Replace with th
 update_css <- function(){
   current_wd <- getwd() #Preserve working directory
   setwd(here::here("SharedResources"))
-  
+
   #Update CSS
   sass(sass_file("textbook.sass"),
        output = paste("textbook.min.css", sep=""),
@@ -15,20 +15,20 @@ update_css <- function(){
          output_style = "compressed",
          source_map_embed = TRUE
        ))
-  
-  
+
+
   folders <- list.dirs("../", recursive = FALSE, full.names = FALSE)
   folders[folders != ".git" & folders != ".Rproj.user" & folders != "SharedResources"]  # Remove folders that don't need css files
-  
+
   for(f in folders){
-    
+
     #If css folder doesn't exist, create it
     if(!file.exists(paste("../", f, "/css", sep="")))
        dir.create(paste("../", f, "/css", sep=""))
-    
+
     #Copy the css file over, replacing existing css file
     file.copy("textbook.min.css", paste("../", f, "/css/textbook.min.css", sep=""), overwrite = TRUE)
-    
+
   }
   setwd(current_wd)#Restore working directory
 }
@@ -43,48 +43,57 @@ update_css <- function(){
 generate_starters <- function(name){
   current_wd <- getwd() #Preserve working directory
   setwd(shared_resources_dir)
-  
+
   #If tutorial's folder doesn't exist, create it
   if(!file.exists(paste("../", name, sep="")))
     dir.create(paste("../", name, sep=""))
-  
+
   #If css folder doesn't exist, create it
   if(!file.exists(paste("../", name, "/css", sep="")))
     dir.create(paste("../", name, "/css", sep=""))
-  
+
   #Copy the css file over, replacing existing css file
   file.copy("textbook.min.css", paste("../", name, "/css/textbook.min.css", sep=""), overwrite = TRUE)
-  
+
   #If images folder doesn't exist create it
   if(!file.exists(paste("../", name, "/images", sep="")))
     dir.create(paste("../", name, "/images", sep=""))
-  
+
   #If the main banner image doesn't exist, copy it
   if(!file.exists(paste("../", name, "/images/Blank Banner.png", sep="")))
     file.copy("images/Blank Banner.png", paste("../", name, "/images/Blank Banner.png", sep=""))
-  
+
   #If the farming dataset's banner image doesn't exist, copy it
   if(!file.exists(paste("../", name, "/images/Blank Dataset.png", sep="")))
     file.copy("images/Blank Dataset.png", paste("../", name, "/images/Blank Dataset.png", sep=""))
-  
+
+  #If data folder doesn't exist create it
+  if(!file.exists(paste("../", name, "/data", sep="")))
+    dir.create(paste("../", name, "/data", sep=""))
+
+  #If the homework dataset (Farming Inventory)doesn't exist, copy it
+  if(!file.exists(paste("../", name, "/data/hmw_data.csv", sep="")))
+    file.copy("datasets/FarmInventory/data.csv", paste("../", name, "/data/hmw_data.csv", sep=""))
+
+
   #If the homework markdown doesn't exist, copy it
   if(!file.exists(paste("../", name, "/homework_1.Rmd", sep="")))
     file.copy("grading/homework_1.Rmd", paste("../", name, "/homework_1.Rmd", sep=""))
-  
+
   #If a tutorial doesn't exist, create it one from the Starter.Rmd
   if(!file.exists(paste("../", name, "/", name, ".Rmd", sep="")))
   {
     file.copy("Starter.Rmd", paste("../", name, sep=""))
-    
+
     #Modify the newly created file so that it replaces all mentions of "starter" with the folder name
     file.rename(paste("../", name, "/Starter.Rmd", sep="") , paste("../", name, "/", name, ".Rmd", sep="") )
     starter  <- readLines(paste("../", name, "/", name, ".Rmd", sep=""))
     f_version  <- gsub(pattern = "_starter_", replace = name, x = starter)
-    writeLines(f_version, con=paste("../", name, "/", name, ".Rmd", sep="")) 
+    writeLines(f_version, con=paste("../", name, "/", name, ".Rmd", sep=""))
   }
-    
+
   setwd(current_wd)#Restore working directory
-  
+
 }
 
 
@@ -93,9 +102,8 @@ generate_starters <- function(name){
 #homework_1.Rmd
 #at the top defines the dataset used:
 # <!-- {{dataset:FarmInventory}} -->
-# <!-- {{datavar:farm_data}} -->
 updateAllHomeworks <- function(hmw_number){
-  
+
 }
 
 #Todo:
