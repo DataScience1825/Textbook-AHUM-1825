f28e288435820b368fcf6e726b8bf6eba0124536
SchneiderCode <schneidm@goldmail.etsu.edu>
2022-09-07 00:39:26 -0400

    file path change

--- R/dataset_descriptions.R
+++ R/dataset_descriptions.R
@@ -5,7 +5,7 @@ insert_description <- function(name){
   if(name == "palmerpenguins"){
     return(
       div(
-        img(src=find_root_file("images", "palmerpenguins_banner.png", criterion= is_r_package), alt="Palmer Penguins, showing drawings of the Chinstrap (left), Gentoo (center), and Adélie (right) penguins", style="width:100%;"),
+        img(src=rprojroot::find_package_root_file("images", "palmerpenguins_banner.png"), alt="Palmer Penguins, showing drawings of the Chinstrap (left), Gentoo (center), and Adélie (right) penguins", style="width:100%;"),
         p("Artwork by ", a(href="https://twitter.com/allison_horst?lang=en", "@allison_horst") )
       )
     )
